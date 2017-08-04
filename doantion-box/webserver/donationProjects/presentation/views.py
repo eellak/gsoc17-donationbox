7182b908825143a995405ede6b678434f8d5c81f
Tas-sos <tasosnumberten@yahoo.gr>
2017-08-04 23:52:31 +0300

    Work on Python Django Wep App. - Basic settings
    * Creating donation-box database
    * Link the database to the presentation page
    * Bug fixes
    * Improved code

--- doantion-box/webserver/donationProjects/presentation/views.py
+++ doantion-box/webserver/donationProjects/presentation/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Donation_Project
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'presentation/index.html')
+    all_projects = Donation_Project.objects.all()
+    template = loader.get_template('presentation/index.html')
+    context =\
+    {
+        'all_projects': all_projects,
+    }
+
+    return HttpResponse( template.render(context, request) )
+
 
