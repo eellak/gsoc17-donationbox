5d542ef9678d8f8ba5caa90290a09aefaef21f8e
Tas-sos <tasosnumberten@yahoo.gr>
2017-08-05 18:23:31 +0300

    Find the next project id

--- doantion-box/webserver/donationProjects/presentation/util/__init__.py
+++ doantion-box/webserver/donationProjects/presentation/util/__init__.py
@@ -20,3 +20,31 @@ def get_first_donation_project():
         raise Http404("Does not exist donation projects yet in the local database! :-) ")
 
     return donation_project
+
+
+def next_project_id( current_id ):
+    """
+    Function that returns the next donation project ID number.
+
+    Attention! The order of projects ids may be unexpected, may not be normal, because there's possible
+    to delete some donation projects. So the order of the donation project ids it ends up having this form :
+    1, 2, 9, 10, 11, 23,.. or
+    18, 23, 24, 25,... from the normal order :
+    1, 2, 3, 4, 5,...
+
+    :param current_id: The id of the current donation project.
+    :return: - The id of the next one donation project, or
+             - The id of the first donation project.
+    """
+    found_the_next_one = False
+
+    for current_project in Donation_Project.objects.all():
+        if found_the_next_one:
+            return current_project.id
+        if current_project.id == current_id:
+            found_the_next_one = True
+
+    return Donation_Project.objects.all().first().id
+
+
+
