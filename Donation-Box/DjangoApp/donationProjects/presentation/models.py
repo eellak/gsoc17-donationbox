dea420c049e5786664f29467dfcc1abf3cda3c14
Tas-sos <tasosnumberten@yahoo.gr>
2017-08-08 09:05:42 +0300

    Add comments to the App model

--- Donation-Box/DjangoApp/donationProjects/presentation/models.py
+++ Donation-Box/DjangoApp/donationProjects/presentation/models.py
@@ -3,6 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Donation_Project( models.Model ):
+    """
+    The Donation_Project model.
+
+    Some observations on the application model and database.
+    Whenever a database entry is updated the "last_updated_datetime" field of the corresponding record is automatically
+    updated. Assigned to this field, the current system timestamp.
+    The beloved Django creates * automatically * and one more field named "id".
+
+    Attention! The fields : "video_URL", "image_URL" and "stylesheet_URL", it may be NULL, but it must not be blank!
+    That is, if it is null (empty), it means they have no value, and not how they may have a zero value ("").
+    Read more about it here: https://docs.djangoproject.com/en/1.11/ref/models/fields/#null
+    """
+
     title                   = models.CharField( max_length=200 )
     text                    = models.TextField()
     current_amount          = models.PositiveIntegerField()
@@ -16,5 +29,7 @@ class Donation_Project( models.Model ):
 
 
 
+
     def __str__(self):
         return self.title
+
