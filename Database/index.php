30aa9dc841eb13ca8a8d31e59b36bb3cbea54e13
Tas-sos <tasosnumberten@yahoo.gr>
2017-08-23 20:14:36 +0300

    Script for receiving data from WordPress page v0.8
    This script must be on the database server side.
    - Requirements:
            * A Web server ( like Apache )
            * Be publicly accessible to an address known to the administrator of the WordPress site.
              ( for example : http://donationbox.database.org/index.php )
    
    This script receives the data that the WordPress site sends, and (for now) writes them in the file "data.log".
    In a future release (οnce the database has been finalized), instead of logging the data into a file,
    it should store them in the main database ("donationbox_network").

---
+++ Database/index.php
@@ -0,0 +1,120 @@
+<?php
+
+	$username				= $_POST['username'];
+	$password				= $_POST['password'];
+
+	$check					= $_POST['check'];
+	$current_amount			= $_POST['current_amount'];
+	$delete					= $_POST['delete'];
+
+	$id						= $_POST['id'];
+	$title					= $_POST['title'];
+	$content				= $_POST['content'];
+	$image_url				= $_POST['image_url'];
+	$video_url				= $_POST['video_url'];
+	$stylesheet_file_url	= $_POST['stylesheet_file_url'];
+	$status					= $_POST['status'];
+	$organization			= $_POST['organization'];
+	$target_amount			= $_POST['target_amount'];
+
+	$start_date				= $_POST['start_date'];
+	$end_date				= $_POST['end_date'];
+	$wordPress_last_modified = $_POST['last_modified'];
+
+
+	if ( isset( $username ) )
+	{
+		if ( $username === 'db_admin' && $password == '123456789' )
+		{
+			// Receive data for delete a donation project
+			if ( isset($_POST['delete']) && $delete == '1' )
+			{
+				header("HTTP/1.1 460 Invalid user credentials");
+
+				$myfile = fopen("data.log", "a+") or die("Unable to open file!");
+
+				$txt = "The donation project \"<b>" . $id ."</b>\" was deleted successfully from donation box database.\n";
+				fwrite($myfile, $txt);
+
+				$txt = "-------------------------------------------------------------------------------------------------\n";
+				fwrite($myfile, $txt);
+
+				fclose($myfile);
+
+				echo "The donation project \"<b>" . $id ."</b>\" was deleted successfully from donation box database.";
+				exit;
+			}
+
+			else if ( isset($_POST['check']) && $check == '1' )
+			{
+				echo '1';
+//				echo 'The user credentials are valid.';
+				exit;
+			}
+
+			else // Receive data for Insert/Update
+			{
+				echo 'I received the data for the donation project "<b>' . $title . '</b>".';
+
+				$myfile = fopen("data.log", "a+") or die("Unable to open file!");
+
+				$txt = $username . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $password . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $id . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $title . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $content . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $image_url . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $video_url . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $stylesheet_file_url . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $status . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $organization . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $target_amount . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $start_date . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $end_date . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = $wordPress_last_modified . "\n";
+				fwrite($myfile, $txt);
+
+				$txt = "-------------------------------------------------------------------------------------------------\n";
+				fwrite($myfile, $txt);
+
+
+				fclose($myfile);
+			}
+		}
+		else // Client error 4xx
+		{
+			header("HTTP/1.1 455 Invalid user credentials");
+			exit;
+		}
+
+	}
+	else 
+		echo 'You did not send any data';
+
+?>
