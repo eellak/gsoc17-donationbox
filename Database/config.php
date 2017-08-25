b5d0a022b86c4be1d57b6a4bd2ab77724de8e652
Tas-sos <tasosnumberten@yahoo.gr>
2017-08-25 16:18:16 +0300

    Basic configuration file for the smooth operation of the index.php file.

---
+++ Database/config.php
@@ -0,0 +1,10 @@
+<?php
+$CONFIG = array (
+  'datadirectory' => '/var/www/',
+  'dbtype' => 'mariaDB',
+  'dbname' => 'donationbox_network',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbuser' => 'db_admin',
+  'dbpassword' => '123456789',
+);
